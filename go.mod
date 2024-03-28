d627ff7585b120dee8668d05c49f057448b4d19e
unknown <joseph.harley02@gmail.com>
2024-03-28 13:51:17 -0400

    created users table

--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module github.com/harljos/gymplanr
 go 1.22.0
 
 require (
+	github.com/joho/godotenv v1.5.1
+	github.com/spf13/cobra v1.8.0
+)
+
+require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/harljos/gymplanr/cmd"
+import (
+	"log"
+	"os"
+
+	"github.com/harljos/gymplanr/cmd"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	godotenv.Load(".env")
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found in the environment")
+	}
+
 	cmd.Execute()
 }
